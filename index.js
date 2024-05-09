e954ffa39a9037b8ca2cc3ee05c233d8b1c4add0
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-05-09 21:37:16 +0530

    Changes related to read and write into the file with help of fs module

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const hello = "Hello World";
-console.log(hello);
+//Imported  the fs module
+const fs = require("fs");
+
+//Used to read file in sync mode. It takes two arguments i.e. path and character encoding
+const readFile = fs.readFileSync("./txt/input.txt", "utf-8");
+// print the output in the console
+console.log(readFile);
+
+//Create a string to write into the file
+const writeFile = `This is what we know about the avacado: ${readFile}.\n Created on ${Date.now()}`;
+
+//Used to write the file in the sync Mode. It take two arguments i.e. path and what needs to be written
+fs.writeFileSync("./txt/output.txt", writeFile);
